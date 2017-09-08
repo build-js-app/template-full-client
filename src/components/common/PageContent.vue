2300ea915564f259d45ac86cfd1176c1a212ad1c
Andrey Temchenko <temchenko32@gmail.com>
2017-09-08 11:28:38 +0300

    auth: do not show page before redirect to login

--- src/components/common/PageContent.vue
+++ src/components/common/PageContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-show="user">
     <Navigation />
 
     <slot></slot>
--- src/components/common/PageContent.vue.js
+++ src/components/common/PageContent.vue.js
@@ -1,6 +1,11 @@
+import {mapGetters} from 'vuex';
+
 import Navigation from '../Navigation.vue';
 
 export default {
   name: 'PageContent',
-  components: {Navigation}
+  components: {Navigation},
+  computed: mapGetters({
+    user: 'user'
+  })
 };
