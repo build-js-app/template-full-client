b2695b7d3088cadd3afeb0640a6efed6b5487cc7
Andrey Temchenko <Andrey-Temchenko@users.noreply.github.com>
2018-01-23 10:19:25 +0200

    misc UI fixes (#1)
    * bug fixes: Sort dropdown not clickable on mobile
    
    * fixes: add label for sort records dropdown
    
    * bug fixes: when add new item sort by current sorting order
    
    * update dependencies

--- src/components/records/RecordsPage.vue
+++ src/components/records/RecordsPage.vue
@@ -2,7 +2,7 @@
   <page-content>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm-12 col-md-11 col-md-offset-1">
+        <div class="col-md-10 col-md-offset-1">
           <div class="row">
             <div class="col-sm-12">
               <h2>Records Page</h2>
